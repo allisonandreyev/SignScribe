0b0f947bdc8c4cb2bebe45ede4de5047dd83feff
sign-scribe <signscribe.official@gmail.com>
2024-07-30 15:18:28 -0400

    Add files via upload

---
+++ parserConfig.py
@@ -0,0 +1,30 @@
+def configParser():
+    file2 = 'Config.txt'
+
+    # Config Variables
+    exitWord=None
+    wordsPause=None
+    lettersPause=None
+    autoSave=None
+    filter1=[]
+    
+    with open(file2, 'r') as file:
+        for line in file:
+            if "exitWord" in line:
+                ls = line.split("\"")
+                exitWord = ls[1]
+            elif "wordsPause" in line:
+                ls = line.split("\"")
+                wordsPause = float(ls[1])
+            elif "lettersPause" in line:
+                ls = line.split("\"")
+                lettersPause = float(ls[1])
+            elif "autoSave" in line:
+                ls = line.split("\"")
+                autoSave = ls[1]
+            elif "Words to censor" in line:
+                ls = line.split("\"")
+                filter1 = ls[1].split()
+    return exitWord, wordsPause, lettersPause, autoSave, filter1
+    
+configParser()
