fc477c22b1b3b7ea6cefa82dd043e765ad271ace
sign-scribe <signscribe.official@gmail.com>
2024-08-07 16:00:58 -0400

    Update UI_Stuff.py

--- GUI/UI_Stuff.py
+++ GUI/UI_Stuff.py
@@ -46,6 +46,7 @@ class VideoPlayer:
     def stop(self):
         self.playing = False
 
+
 def populate_queue(text_queue, wordBacklog):
     for _ in range(10):
         formatted_text = format_text(wordBacklog)
@@ -59,15 +60,65 @@ def format_text(wordBacklog):
     updated_text = '\n'.join(lines)
     return updated_text
 
-def update_hand(hand_queue):
-    base_path = r"C:\Users\user\Downloads\Hand_signs"
-    if hand_queue in 'abcdefghijklmnopqrstuvwxyz':
-        return fr"{base_path}\{hand_queue.upper()}.mp4"
-    else:
-        return fr"{base_path}\open_palm.mp4"
+def update_hand_letter(hand_queue):
+    match hand_queue:
+        case 'a':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/A.mp4"
+        case 'b':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/B.mp4"
+        case 'c':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/C.mp4"    
+        case 'd':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/D.mp4"
+        case 'e':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/E.mp4"
+        case 'f':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/F.mp4"
+        case 'g':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/G.mp4"
+        case 'h':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/H.mp4"
+        case 'i':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/I.mp4"
+        case 'j':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/J.mp4"
+        case 'k':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/K.mp4"
+        case 'l':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/L.mp4"
+        case 'm':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/M.mp4"
+        case 'n':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/N.mp4"
+        case 'o':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/O.mp4"
+        case 'p':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/P.mp4"
+        case 'q':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/Q.mp4"
+        case 'r':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/R.mp4"
+        case 's':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/S.mp4"
+        case 't':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/T.mp4"
+        case 'u':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/U.mp4"
+        case 'v':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/V.mp4"
+        case 'w':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/W.mp4"
+        case 'x':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/X.mp4"
+        case 'y':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/Y.mp4"
+        case 'z':
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/Z.mp4"
+        case _:
+            return r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/A.mp4"
             
 
-def GUI(text_queue, hand_queue, letter_queue, wordBacklog):
+def GUI(text_queue, hand_queue):
 
     window = tk.Tk()
     window.geometry('%dx%d+0+0' % (window.winfo_screenwidth(), window.winfo_screenheight()))
@@ -83,15 +134,15 @@ def GUI(text_queue, hand_queue, letter_queue, wordBacklog):
     text_frame = tk.Frame(window, bg='#474747')
     text_frame.place(x=0, y=window_height // 2, width=window_width // 2, height=window_height // 2)
 
-    player = VideoPlayer(video_frame, r"C:\Users\user\Downloads\Hand_signs\O.mp4", window)
+    player = VideoPlayer(video_frame, r"/home/signscribe/Downloads/SignScribe-master/Hand_signs/open_palm.mp4", window)
 
     scrollbar = tk.Scrollbar(text_frame, bg="#474747")
     scrollbar.pack(side="right", fill="y")
 
-    textbox = tk.Text(text_frame, bg="#474747", fg="#FFFFFF", wrap="word", yscrollcommand=scrollbar.set, font=('Courier', 18))
+    textbox = tk.Text(text_frame, bg="#474747", fg="#FFFFFF", wrap="word", yscrollcommand=scrollbar.set, font=('monospace', 18))
     textbox.pack(fill="both", expand=True)
     scrollbar.config(command=textbox.yview, bg="#474747")
-
+    
     # Define the UI frame
     UI_frame = tk.Frame(window, bg='#474747')
     UI_frame.place(x=window_width/2, y=0, width=window_width // 2, height=window_height)
@@ -99,7 +150,7 @@ def GUI(text_queue, hand_queue, letter_queue, wordBacklog):
     # Define the dimensions for Letter_update_frame
     frame_width = (window_width // 2) - (window_width // 30)
     frame_height = (window_height // 3) - (window_height // 30)
-
+    
     # Calculate the x position to center Letter_update_frame in UI_frame
     x_position = ((window_width // 2) - frame_width) // 2
 
@@ -120,53 +171,42 @@ def GUI(text_queue, hand_queue, letter_queue, wordBacklog):
 
     Letter_show = tk.Text(Letter_update_frame, bg ="#474747", fg = "#FFFFFF", wrap="word", font=('Courier', 120))
     Letter_show.place(x = 250, y = 40, width=200, height=200)
-    
+
     def update_text():
         try:
             new_text = text_queue.get_nowait()
-            wordBacklog.clear()
-            wordBacklog.extend(new_text.split())
             textbox.delete(1.0, tk.END)
             textbox.insert(tk.END, new_text)
-
         except queue.Empty:
             pass
-
+        
         try:
             hand_sign = hand_queue.get_nowait()
-            current_word = letter_queue.get_nowait()
-            update_hand(hand_sign, current_word)
+            update_hand(hand_sign)
         except queue.Empty:
             pass
 
         window.after(100, update_text)
 
-    def update_hand(hand_sign, current_word):
-        video_path = f"C:\\Users\\user\\Downloads\\Hand_signs\\{hand_sign}.mp4"
+    def update_hand(hand_sign):
+        video_path = update_hand_letter(hand_sign)
         player.set_path(video_path)
         Letter_show.delete(1.0, tk.END)  # Clear the previous hand_sign
         Letter_show.insert(tk.END, hand_sign.upper())  # Insert the current hand_sign
-        highlight_text(current_word)
+        highlight_text(hand_sign)
 
-    def highlight_text(current_word):
+    def highlight_text(hand_sign):
         text = textbox.get("1.0", tk.END)
-        textbox.tag_remove("highlight", "1.0", tk.END)
-
-        start_index = '1.0'
-        while True:
-            start_index = textbox.search(current_word, start_index, tk.END)
-            if not start_index:
-                break
-            end_index = f"{start_index}+{len(current_word)}c"
-            textbox.tag_add("highlight", start_index, end_index)
+        index = text.lower().find(hand_sign.lower())
+        if index != -1:
+            textbox.tag_remove("highlight", "1.0", tk.END)
+            textbox.tag_add("highlight", f"1.{index}", f"1.{index+1}")
             textbox.tag_config("highlight", background="yellow", foreground="black")
-            start_index = end_index
-            break  # Only highlight the first occurrence
 
     window.after(100, update_text)
     window.mainloop()
 
-
-def GUI_APP(text_queue, wordBacklog, hand_queue, letter_queue):
+def GUI_APP(text_queue, wordBacklog, hand_queue):
     populate_queue(text_queue, wordBacklog)
-    GUI(text_queue, hand_queue, letter_queue, wordBacklog)
+    GUI(text_queue, hand_queue)
+
