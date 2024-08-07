053aca12b7400f591109f629b275bf813202c03b
Allison Andreyev <137202319+allisonandreyev@users.noreply.github.com>
2024-08-07 14:00:31 -0400

    added comments to handControl.py

--- Code Files/HandControl.py
+++ Code Files/HandControl.py
@@ -3,6 +3,8 @@ from adafruit_servokit import ServoKit
 
 ServoController = ServoKit(channels = 16)
 
+# function definition. This function takes in the angle measurements for all 7 servo movements, with default values being the high five position and controls the servos to move to that angle position.
+# default values can be changed, but must be 10 <= X <= 170 or the servos will be stressed.
 def controlServo(serv1 = 0, serv2 = 0, serv3 = 0, serv4 = 0, serv5 = 0, serv6 = 90, serv7 = 90):
 	ServoController.servo[1].angle = serv1
 	ServoController.servo[2].angle = serv2
@@ -11,5 +13,6 @@ def controlServo(serv1 = 0, serv2 = 0, serv3 = 0, serv4 = 0, serv5 = 0, serv6 = 
 	ServoController.servo[5].angle = serv5
 	ServoController.servo[6].angle = serv6
 	ServoController.servo[7].angle = serv7
-	
+
+#calls the function for testing, now used in SignScribeOfficial.py file
 #controlServo(serv1 = 160, serv2 = 0, serv3 = 0, serv4 = 0, serv5 = 0, serv6 = 90, serv7 = 90)
